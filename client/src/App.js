a3644a93eef403a12b142a19f81603695ef97abc
Louis Klein <louisklein71@gmail.com>
2023-08-27 18:54:41 -0700

    react init

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {createContext, useState, useEffect} from 'react';
+import {Routes, Route} from 'react-router-dom';
+
+import Home from './components/home';
+
+export const AuthContext = createContext();
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthContext.Provider value={{ token, setToken, logout , setUserData, userData, guilds, setGuilds, sessionData, setSessionData}}>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </AuthContext.Provider>
   );
 }
 
