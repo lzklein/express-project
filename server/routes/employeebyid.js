e8aad2f6a170f14da0ca51d794a02331f0fddff7
Louis Klein <louisklein71@gmail.com>
2023-08-30 18:26:35 -0700

    employee routes done

--- server/routes/employeebyid.js
+++ server/routes/employeebyid.js
@@ -9,6 +9,7 @@ const Employee = require('../models/employees'); // Import your Employee model
 // GET employee by ID
 router.get('/:id', async (req, res) => {
   const id = req.params.id;
+  
   try {
     const employee = await Employee.findByPk(id);
     if (employee) {
@@ -18,6 +19,41 @@ router.get('/:id', async (req, res) => {
     }
   } catch (error) {
     res.status(500).json({ message: 'Error retrieving employee' });
+  }
+});
+
+// PATCH update employee
+router.patch('/employees/:id', async (req, res) => {
+  const id = req.params.id;
+  const updates = req.body; 
+
+  try {
+    const employee = await Employee.findByPk(id);
+    if (employee) {
+      await employee.update(updates);
+      res.json({ message: 'Employee updated successfully' });
+    } else {
+      res.status(404).json({ message: 'Employee not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Error updating employee' });
+  }
+});
+
+// DELETE employee
+router.delete('/employees/:id', async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const employee = await Employee.findByPk(id);
+    if (employee) {
+      await employee.destroy();
+      res.json({ message: 'Employee deleted successfully' });
+    } else {
+      res.status(404).json({ message: 'Employee not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Error deleting employee' });
   }
 });
 
--- server/routes/employees.js
+++ server/routes/employees.js
@@ -2,15 +2,26 @@
 // post(hire) new employee
 const express = require('express');
 const router = express.Router();
-const Employee = require('../models/employees'); // Import your Employee model
+const Employee = require('../models/employees'); 
 
 // GET all employees
-router.get('/', async (req, res) => {
+router.get('/employees', async (req, res) => {
   try {
     const employees = await Employee.findAll();
     res.json(employees);
   } catch (error) {
     res.status(500).json({ message: 'Error retrieving employees' });
+  }
+});
+
+// POST employee
+router.post('/employees', async (req, res) => {
+  const newEmployee = req.body;
+  try {
+    const createdEmployee = await Employee.create(newEmployee);
+    res.status(201).json(createdEmployee); 
+  } catch (error) {
+    res.status(500).json({ message: 'Error creating employee' });
   }
 });
 
