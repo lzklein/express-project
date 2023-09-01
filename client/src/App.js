5c532f461ecab9c1bffbd8a85917fa7231409425
Louis Klein <louisklein71@gmail.com>
2023-08-31 20:01:59 -0700

    navbar routes built

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,16 @@ import './App.css';
 import React, {createContext, useState, useEffect} from 'react';
 import {Routes, Route} from 'react-router-dom';
 
+// Components
 import Home from './components/home';
-import Header from './components/header'
+import Header from './components/header';
+import Timeoff from './components/timeoff';
+import Schedule from './components/schedule';
+import Shifts from './components/shifts';
+import Employees from './components/employees';
+import Login from './components/login';
+import Profile from './components/profile';
+
 export const AuthContext = createContext();
 
 function App() {
@@ -14,6 +22,12 @@ function App() {
       <div className="App">
         <Header />
         <Routes>
+          <Route path='/timeoff' element={<Timeoff />}/>
+          <Route path='/schedule' element={<Schedule />}/>
+          <Route path='/shifts' element={<Shifts />}/>
+          <Route path='/employees' element={<Employees />}/>
+          <Route path='/profile' element={<Profile />}/>
+          <Route path='/login' element={<Login />}/>
           <Route path="/" element={<Home />} />
         </Routes>
       </div>
---
+++ client/src/components/shifts.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// make edit delete shifts
+const shifts = () => {
+  return (
+    <div>shifts</div>
+  )
+}
+
+export default shifts
